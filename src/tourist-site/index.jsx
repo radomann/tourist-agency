46e09dcbbd2e385b733dc47971b4d5a6f96d8249
Nemanja Radoman <nemanja.radoman@gmail.com>
2023-10-07 18:57:38 +0200

    FIX CSS

--- src/tourist-site/index.jsx
+++ src/tourist-site/index.jsx
@@ -1,38 +1,38 @@
-import { useState } from 'react';
-import '../App.css'
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'jquery/dist/jquery.min.js'
-import 'bootstrap/dist/js/bootstrap.min.js'
-import 'mdb-react-ui-kit/dist/css/mdb.min.css';
-import "@fortawesome/fontawesome-free/css/all.min.css";
+import { useState } from "react";
+import "../App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "jquery/dist/jquery.min.js";
+import "bootstrap/dist/js/bootstrap.min.js";
+import "mdb-react-ui-kit/dist/css/mdb.min.css";
+import "fortawesome/fontawesome-free/css/all.min.css";
 import "react-image-gallery/styles/css/image-gallery.css";
-import { UserProvider } from '../context/UserContext'
-import { AppLayout } from './layout';
-import { HomePage } from './pages/home'
-import { appRoutes } from '../router/router';
-import { RouterProvider, createBrowserRouter } from 'react-router-dom';
-import { FooterPage } from './layout/footer';
+import { UserProvider } from "../context/UserContext";
+import { AppLayout } from "./layout";
+import { HomePage } from "./pages/home";
+import { appRoutes } from "../router/router";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import { FooterPage } from "./layout/footer";
 
 export const App = () => {
   const router = createBrowserRouter(appRoutes);
 
   return (
     <>
-    <AppLayout />
-    <HomePage /> 
-    <FooterPage />
+      <AppLayout />
+      <HomePage />
+      <FooterPage />
     </>
-  )
-}
+  );
+};
 
-// /offers/ - Admin ima pravo za sve CRUD operacije, a obican korisnik ili neulogovani korisnik samo da vidi ponude. 
+// /offers/ - Admin ima pravo za sve CRUD operacije, a obican korisnik ili neulogovani korisnik samo da vidi ponude.
 // Polja za offer su id, title, description, price, duration, start_date, end_date, category, media.
 
 // /categories/ - Samo admin moze da pristupi i radi CRUD operacije
 // Polja za category su id, name
 
 // /reservations/ - Samo autentifikovani korisnik moze da dodaje rezervaciju, i gleda svoje rezervacije.
-// user se popunjava automatski, a offer se bira i salje id. 
+// user se popunjava automatski, a offer se bira i salje id.
 // Polja za reservation su id, offer, user
 
 // /testimonials/ - Samo autentifikovani korisnik moze da dodaje,
